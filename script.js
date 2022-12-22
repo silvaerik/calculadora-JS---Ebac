d59f47b2807976cbadb92143d1c4e1e9a25ecea5
Erik Ricardo da Silva <88250182+silvaerik@users.noreply.github.com>
2022-12-22 12:49:56 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let horasDiarias = document.querySelector("#horas-diarias");
+let diasMes = document.querySelector("#dias-mes");
+let despesas = document.querySelector("#despesas");
+let equipamento = document.querySelector("#equipamento");
+
+let resultado = document.querySelector("#valor-total");
+let hidden = document.querySelector("#resultado");
+
+cons enviar = document.querySelector("#calcular");
+
+enviar.addEventlistener("click",function(){
+  
+  if(horasDiarias.value == "" ||)
+})
